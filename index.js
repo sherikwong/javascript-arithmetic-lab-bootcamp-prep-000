1bb5c9d90444c9e0b0b083816cf915dc25dcc4fb
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function divide(a, b) {
   5.0 / 2.5;
 }
 
-var number = 5;
+var number = 10;
 
-number++
+function add5() {
+  number += 5;
+}
+
+function divideBy3() {
+  number /= 3;
+}
+
+divideBy3();
+console.log(number);
+add5();
+console.log(number);
+number = 10;
+add5();
+console.log(number);
+divideBy3();
+console.log(number);
