17246099e5d4c8051f00dbcdc5eef738810ed8ab
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,17 @@ function dec(n) {
 }
 
 function makeInt(n, base) {
-
+  if () {
+    
+  }
   return parseInt(n, base);
   
 }
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
 
 function preserveDecimal(n) {
   return parseFloat(n);
