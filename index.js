a3ce8925b34f99a7a4ddeceb040f98e33a420930
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 function add(a, b) {
-  1 + 80;
+  return a + b;
 }
-
-function subtract(a, b) {
-  60 - 40;
-}
-
-function multiply(a, b) {
-  2 * 3.4;
-}
-
-function divide(a, b) {
-  5.0 / 2.5;
-}
-
-var number = 10;
-
-function add5() {
-  number += 5;
-}
-
-function divideBy3() {
-  number /= 3;
-}
-
-divideBy3();
-console.log(number);
-add5();
-console.log(number);
-number = 10;
-add5();
-console.log(number);
-divideBy3();
-console.log(number);
-
-parseInt('2.2222', 10);
-parseInt('nonsense!', 10);
-parseFloat('80.123999');
 
 
 var a, b
