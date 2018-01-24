fca9ab9114099c678222f355f076b6455aa62509
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,28 +3,19 @@ function add(a, b) {
 }
 
 
-var a, b
+function subtract(a, b) {
+  return a - b;
+}
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+function multiply(a, b) {
+  return a * b;
+}
 
-it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)).toEqual(a + b)
-})
+function divide(a, b) {
+  return a / b;
+}
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
